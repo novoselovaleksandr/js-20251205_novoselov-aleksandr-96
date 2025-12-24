1cc62ab539580411efd557464d04a55bfd158908
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2025-12-24 23:22:06 +0500

    [master] component

---
+++ components/component.js
@@ -0,0 +1,15 @@
+export class Component {
+    #element = null;
+
+    constructor(tagName = 'div') {
+      this.#element = document.createElement(tagName);
+    }
+
+    set html(content) {
+      this.#element.innerHTML = content;
+    }
+
+    get element() {
+      return this.#element;
+    }
+}
