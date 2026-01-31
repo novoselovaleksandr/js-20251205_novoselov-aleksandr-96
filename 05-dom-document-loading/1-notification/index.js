a77fe6e9c08960bed9a0e9f8430ee8a9262475b8
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-01-31 22:57:47 +0500

    [master] initial notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,16 @@
-export default class NotificationMessage {
+import { Component } from '../../components/component.js';
+export default class NotificationMessage extends Component {
+  constructor() {
+    super();
 
+    this.render();
+  }
+
+  template() {
+    return ``;
+  }
+
+  render() {
+    this.html = this.template();
+  }
 }
