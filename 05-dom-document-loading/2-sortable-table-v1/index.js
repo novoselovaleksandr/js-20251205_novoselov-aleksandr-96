d81917bfef6e2d535d8eac6607ed094d7ebf7ea7
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-02-01 18:58:04 +0500

    [master] headerColumns + bodyColumns

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -12,15 +12,41 @@ export default class SortableTable extends Component {
     this.render();
   }
 
+  #headerColumns() {
+    return this.#headerConfig?.lenght === 0 ? `` : `
+      ${this.#headerConfig.map(column => `
+        <div class="sortable-table__cell" data-id="${column.id}" data-sortable="${column.sortable}" data-order="asc">
+          <span>${column.title}</span>
+          <span data-element="arrow" class="sortable-table__sort-arrow">
+            <span class="sort-arrow"></span>
+          </span>
+        </div>
+      `).join('\n')}
+    `; 
+  }
+
+  #bodyColumns() {
+    return this.#headerConfig?.length === 0 || this.#data?.length === 0 ? `` : `
+      ${this.#data.map(row => 
+      this.#headerConfig.map(cell => {
+        const cellData = row[cell.id];
+          
+        return cell.template ? cell.template(cellData) : `<div class="sortable-table__cell">${cellData}</div>`;
+      }).join('\n')
+    ).join('\n')}`;
+  }
+
   template() {
     return `
       <div data-element="productsContainer" class="products-list__container">
         <div class="sortable-table">
           <div data-element="header" class="sortable-table__header sortable-table__row">
-
+            ${this.#headerColumns()}
           </div>
           <div data-element="body" class="sortable-table__body">
-
+            <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
+              ${this.#bodyColumns()}
+            </a>
           </div>
         </div>
       </div>
