3f0b8c1390b34c5391cd345e41e19e1a1b7c2440
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-02-01 15:53:53 +0500

    [master] init SortableTable

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,23 @@
-export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+import { Component } from "../../components/component.js";
+export default class SortableTable extends Component {
+  #headerConfig = [];
+  #data = [];
 
+  constructor(headerConfig = [], data = []) {
+    super();
+
+    this.#headerConfig = headerConfig;
+    this.#data = data;
+
+    this.render();
+  }
+
+  template() {
+    return ``;
+  }
+
+  render() {
+    this.html = this.template();
   }
 }
 
