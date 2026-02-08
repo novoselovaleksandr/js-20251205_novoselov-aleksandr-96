94c2d4d15f6375fbd431084cf9ae52b01acd98ed
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-02-08 12:55:02 +0500

    [master] init tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,21 @@
-class Tooltip {
+import { Component } from "../../components/component.js";
+
+class Tooltip extends Component {
+  constructor() {
+    super();
+    this.#render();
+  }
+
   initialize () {
 
   }
-}
 
+  #template() {
+
+  }
+
+  #render() {
+    this.html = this.#template();
+  }
+}
 export default Tooltip;
