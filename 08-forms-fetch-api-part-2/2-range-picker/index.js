207a825fba3b23905140f5a07cf86007a240a8ce
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-02-22 19:45:15 +0500

    [master] refactoring

--- 08-forms-fetch-api-part-2/2-range-picker/index.js
+++ 08-forms-fetch-api-part-2/2-range-picker/index.js
@@ -1,27 +1,65 @@
 import { Component } from "../../components/component.js";
 
 export default class RangePicker extends Component {
-    from= null;
-    to=null;
+  from = null;
+  to = null;
+  isOpen = false;
+  #rangepickerInput = null;
+  #boundRangepickerClickHandler = null
+  #rangepickerSelector = null
 
-    constructor({ from, to } = {}) {
-      super();
-      this.from = from ?? this.from;
-      this.to = to ?? this.to;
+  constructor({ from, to } = {}) {
+    super();
+    this.from = from ?? this.from;
+    this.to = to ?? this.to;
 
-      this.render();
-    }
+    this.render();
 
-    render() {
-      this.html = this.template();
-    }
+    this.#rangepickerInput = this.element.querySelector('.rangepicker__input');
+    this.#boundRangepickerClickHandler = this.rangepickerClickHandler.bind(this);
 
-    template() {
-      // Форматируем даты для отображения в input
-      const fromStr = this.from ? this.formatDate(this.from) : '';
-      const toStr = this.to ? this.formatDate(this.to) : '';
+    this.#rangepickerSelector = this.element.querySelector('.rangepicker__selector');
 
-      return `
+    this.#initListeners();
+  }
+
+  render() {
+    this.html = this.template();
+  }
+
+  #initListeners() {
+    this.#rangepickerInput.addEventListener('click', this.#boundRangepickerClickHandler);
+  }
+
+  #removeListeners() {
+    this.#rangepickerInput.removeEventListener('click', this.#boundRangepickerClickHandler);
+  }
+
+  destroy() {
+    this.#removeListeners();
+    super.destroy();
+  }
+
+  rangepickerClickHandler() {
+    this.isOpen = !this.isOpen;
+    this.element.classList.toggle('rangepicker_open');
+    
+    if (this.isOpen) {
+      this.renderSelector();
+    } 
+
+
+  }
+
+  renderSelector() {
+    this.#rangepickerSelector.innerHTML = this.selectorTemplate();
+  }
+
+  template() {
+    const fromStr = this.from ? this.formatDate(this.from) : '';
+    const toStr = this.to ? this.formatDate(this.to) : '';
+
+    return `
       <div class="rangepicker">
         <div class="rangepicker__input">
           <span>${fromStr}</span>
@@ -31,12 +69,34 @@ export default class RangePicker extends Component {
         <div class="rangepicker__selector"></div>
       </div>
     `;
-    }
+  }
 
-    formatDate(date) {
-      const day = String(date.getDate()).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const year = date.getFullYear();
-      return `${day}.${month}.${year}`;
-    }
+  selectorTemplate() {
+    return `
+      <div class="rangepicker__selector-arrow"></div>
+      <div class="rangepicker__calendar">
+        <div class="rangepicker__month-indicator"></div>
+        <div class="rangepicker__selector-control-left"></div>
+        <div class="rangepicker__selector-control-right"></div>
+        <div class="rangepicker__day-of-week">
+          <div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div>
+        </div>
+        <div class="rangepicker__date-grid"></div>
+      </div>
+      <div class="rangepicker__calendar">
+        <div class="rangepicker__month-indicator"></div>
+        <div class="rangepicker__day-of-week">
+          <div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div>
+        </div>
+        <div class="rangepicker__date-grid"></div>
+      </div>
+    `;
+  }
+
+  formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
 }
